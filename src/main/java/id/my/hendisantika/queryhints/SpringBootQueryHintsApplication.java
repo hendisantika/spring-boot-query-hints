16a53226288b216f25f06d9f124b0b79cd67bde9
hendisantika <hendisantika@yahoo.co.id>
2025-03-08 06:21:08 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/queryhints/SpringBootQueryHintsApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.queryhints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQueryHintsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQueryHintsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/queryhints/SpringBootQueryHintsApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.queryhints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQueryHintsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
